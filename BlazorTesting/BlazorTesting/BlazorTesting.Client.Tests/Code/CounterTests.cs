ff123b16d796f248454fbe706b7182fed4889945
Alberto Mori <albi.mo87@hotmail.it>
2020-07-27 15:51:37 +0200

    setup bunit, add some unit tests to counter component

---
+++ BlazorTesting/BlazorTesting/BlazorTesting.Client.Tests/Code/CounterTests.cs
@@ -0,0 +1,33 @@
+﻿using BlazorTesting.Client.Pages;
+using Bunit;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace BlazorTesting.Client.Tests.Code
+{
+    public class CounterTests : TestContext
+    {
+        [Fact]
+        public void Counter_Component_Should_Initialize_With_Counter_Zero()
+        {
+            var component = RenderComponent<Counter>();
+            
+            var paragraph = component.Find("p");
+            paragraph.MarkupMatches("<p>Current count: 0</p>");
+        }
+
+        [Fact]
+        public void Counter_Component_Should_Increment_Current_Count_When_Button_Is_Clicked()
+        {
+            var component = RenderComponent<Counter>();
+            
+            var button = component.Find("button");
+            button.Click();
+
+            var paragraph = component.Find("p");
+            paragraph.MarkupMatches("<p>Current count: 1</p>");
+        }
+    }
+}
