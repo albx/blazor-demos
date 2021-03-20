f03df335570cb0ddae3064d445e148a4891f15b0
Alberto Mori <albi.mo87@hotmail.it>
2021-03-20 17:14:19 +0100

    bUnit getting started

---
+++ BlazorTesting/src/BlazorTesting.App/Services/ITaskService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorTesting.App.Services
+{
+    public interface ITaskService
+    {
+        Task<List<string>> LoadItemsAsync();
+
+        Task AddNewItemAsync(string newItemText);
+    }
+}
---
+++ BlazorTesting/src/BlazorTesting.App/Services/TaskService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorTesting.App.Services
+{
+    public class TaskService : ITaskService
+    {
+        private List<string> items = new();
+
+        public Task AddNewItemAsync(string newItemText)
+        {
+            items.Add(newItemText);
+            return Task.CompletedTask;
+        }
+
+        public Task<List<string>> LoadItemsAsync() => Task.FromResult(items);
+    }
+}
